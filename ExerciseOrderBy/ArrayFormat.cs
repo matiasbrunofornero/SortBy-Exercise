14e4b27a5fce82a3fde1cdfe369d2b68efc497a9
Matias Bruno <matiasbrunofornero@gmail.com>
2019-08-04 13:03:21 -0300

    Summary comment added before each method in order to let explain the functionality of the methods with a summary, param and returns tag inside it and allow the documentation of the methods inside the code.

--- ExerciseOrderBy/ArrayFormat.cs
+++ ExerciseOrderBy/ArrayFormat.cs
@@ -6,12 +6,27 @@ namespace ExerciseOrderBy
 {
     public class ArrayFormat
     {
+        /// <summary>
+        /// Gets a string by parameter, FormatSpecialCharacters method will remove all characters 
+        /// that do not match with the regular expression and replace it with empty characters (space).
+        /// </summary>
+        /// <param name="text">Text that will be formatted</param>
+        /// <returns>This method will return the original string without any special characters</returns>
+
         public string FormatSpecialCharacters(string text)
         {
             Regex expression = new Regex("[^a-z0-9]");
             text = expression.Replace(text, " ");
             return text;
         }
+
+        /// <summary>
+        /// Gets a string by parameter, TextToFormattedArray method will convert the string in a string array. 
+        /// After special characters clean, the formatted text will be separated in different strings that 
+        /// string will be separated between them taking the space character as limit.
+        /// </summary>
+        /// <param name="text">Text that will be convert to array</param>
+        /// <returns>This method will return the original string converted in a string array</returns>
 
         public string[] TextToFormattedArray(string text)
         {
@@ -20,6 +35,12 @@ namespace ExerciseOrderBy
                 StringSplitOptions.RemoveEmptyEntries);
             return array;
         }
+
+        /// <summary>
+        /// Gets a array by parameter, OrderByAscending method will order the array respecting a rule. 
+        /// </summary>
+        /// <param name="text">Array that will be ordered follow a rule</param>
+        /// <returns>This method will return the original array alphabetically ordered</returns>
 
         public string[] OrderByAscending(string[] array)
         {
